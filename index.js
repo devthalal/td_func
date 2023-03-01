da065c17454f109e44c40b532644332240c6e097
devthalal <thalal.hassan@neoito.com>
2023-03-01 15:38:15 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-
+import { shared } from "@appblocks/node-sdk";
 
 const handler = async (req, res) => {
-
   // health check
   if (req.params["health"] === "health") {
-    res.write(JSON.stringify({success: true, msg: "Health check success"}))
-    res.end()
+    res.write(JSON.stringify({ success: true, msg: "Health check success" }));
+    res.end();
   }
 
-  // Add your code here
-  res.write(JSON.stringify({success: true, msg: `Happy Hacking`}))
-  res.end()
-  
-}
+  const { helper } = await shared.getShared();
 
-export default handler
+  // Add your code here
+  res.write(
+    JSON.stringify({ success: true, msg: `Happy Hacking`, helper: helper() })
+  );
+  res.end();
+};
+
+export default handler;
